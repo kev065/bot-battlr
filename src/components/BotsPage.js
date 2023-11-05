fa5ed721d64986d50bbaecb4f0724373a1bddd14
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-05 12:03:37 +0300

    updated

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,51 @@
-import React from "react";
+import {useState} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
-function BotsPage() {
-  //start here with your code for step one
+function BotsPage({ botCollection, setBotCollection, setBotCollectionState }) {
+    const [botBasket, setBotBasket] = useState([]);
 
-  return (
-    <div>
-      <YourBotArmy />
-      <BotCollection />
-    </div>
-  )
+    const addBotToBasket = (bot) => {
+        const botInBasket = botBasket.find((item) => item.id === bot.id);
+        if (!botInBasket) {
+            setBotBasket([...botBasket, bot]);
+        }
+    };
+    const removeBotFromBasket = (bot) => {
+        const botInBasket = botBasket.find((item) => item.id === bot.id);
+        if (botInBasket) {
+            setBotBasket(
+                botBasket.filter((filteredBot) => filteredBot.id !== bot.id)
+            );
+        }
+    };
+    function dischargeBot(bot) {
+        setBotCollection(botCollection.filter((item) => item.id !== bot.id));
+        removeBotFromBasket(bot)
+        fetch(`http://localhost:3000/bots/${bot.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        setBotCollectionState(true);
+
+    }
+    return (
+        <div>
+            <YourBotArmy
+                botBasket={botBasket}
+                removeBotFromBasket={removeBotFromBasket}
+                dischargeBot={dischargeBot}
+            />
+            <BotCollection
+                botCollection={botCollection}
+                addBotToBasket={addBotToBasket}
+                dischargeBot={dischargeBot}
+            />
+        </div>
+    );
 }
 
 export default BotsPage;
+
