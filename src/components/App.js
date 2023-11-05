87736477efdcdb213282ae96e4a0880d32e91d27
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-05 11:54:51 +0300

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import BotsPage from "./BotsPage";
 
 function App() {
-  return (
-    <div className="App">
-      <BotsPage />
-    </div>
-  );
-}
+    const [botCollection, setBotCollection] = useState([]);
+    const [botCollectionState, setBotCollectionState] = useState(false);
 
+    useEffect(() => {
+        const controller = new AbortController();
+        fetch("http://localhost:3000/bots", { signal: controller.signal })
+            .then((res) => res.json())
+            .then((bots) => {
+                console.log(bots);
+                setBotCollection(bots);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+        return () => {
+            controller.abort();
+        };
+    }, [botCollectionState]);
+
+    return (
+        <div className="App">
+            <BotsPage
+                botCollection={botCollection}
+                setBotCollection={setBotCollection}
+                setBotCollectionState={setBotCollectionState}
+            />
+        </div>
+    );
+}
 export default App;
